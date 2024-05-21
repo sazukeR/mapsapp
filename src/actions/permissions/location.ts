1d039388eeebe099efa7e501a1294fc9fb4f530d
Reinaldo Contreras <reinaldojcg1@gmail.com>
2024-05-21 17:37:03 -0500

    Add request permission

---
+++ src/actions/permissions/location.ts
@@ -0,0 +1,33 @@
+import { PERMISSIONS, PermissionStatus as RNPermissionsStatus, openSettings, request } from "react-native-permissions"
+import type { PermissionStatus } from "../../infrastructure/interfaces/permissions";
+import { Platform } from "react-native";
+
+
+export const requestLocationPermissions = async(): Promise<PermissionStatus> => {
+
+
+ let status: RNPermissionsStatus = "unavailable";
+
+ if ( Platform.OS === 'ios' ) {
+  status = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+ } else if ( Platform.OS === 'android' ) {
+  status = await request( PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION );
+ } else {
+  throw new Error('Unsupported platform');
+ }
+
+ if (status === 'blocked') {
+  await openSettings();
+  // return await // checkLocationPermission();
+ }
+
+ const permissionMapper: Record<RNPermissionsStatus, PermissionStatus> = {
+   granted: "granted",
+   denied: "denied",
+   blocked: "blocked",
+   unavailable: "unavailable",
+   limited: 'limited',
+ };
+
+ return permissionMapper[status] ?? 'unavailable';
+}
