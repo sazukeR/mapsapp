83e8eb742ade167865b59d011af028521252815f
Reinaldo Contreras <reinaldojcg1@gmail.com>
2024-05-21 15:38:49 -0500

    Add initial settings permissions on android

--- src/MapsApp.tsx
+++ src/MapsApp.tsx
@@ -2,11 +2,12 @@ import 'react-native-gesture-handler';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {Text} from 'react-native';
+import {StackNavigator} from './presentation/navigation/StackNsavigator';
 
 export const MapsApp = () => {
   return (
     <NavigationContainer>
-      <Text>MapsApp</Text>
+      <StackNavigator />
     </NavigationContainer>
   );
 };
