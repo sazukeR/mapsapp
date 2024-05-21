affba4e7796edff3d26257080ab1e1a6243e98b7
Reinaldo Contreras <reinaldojcg1@gmail.com>
2024-05-21 15:26:49 -0500

    Add navigation

---
+++ src/MapsApp.tsx
@@ -0,0 +1,12 @@
+import 'react-native-gesture-handler';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {Text} from 'react-native';
+
+export const MapsApp = () => {
+  return (
+    <NavigationContainer>
+      <Text>MapsApp</Text>
+    </NavigationContainer>
+  );
+};
