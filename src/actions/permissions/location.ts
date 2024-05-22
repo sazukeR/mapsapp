3867f8cec0eb8ba3fda25369845478500199b93b
Reinaldo Contreras <reinaldojcg1@gmail.com>
2024-05-22 11:25:27 -0500

    Add check location permissions

--- src/actions/permissions/location.ts
+++ src/actions/permissions/location.ts
@@ -30,4 +30,34 @@ export const requestLocationPermissions = async(): Promise<PermissionStatus> => 
  };
 
  return permissionMapper[status] ?? 'unavailable';
+ 
+}
+
+
+
+
+
+
+export const checkLocationPermissions = async():Promise<PermissionStatus> => {
+
+ let status: RNPermissionsStatus = 'unavailable';
+
+ if ( Platform.OS === 'ios' ) {
+  status = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+ } else if ( Platform.OS === 'android' ) {
+  status = await request( PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION );
+ } else {
+  throw new Error('Unsupported platform');
+ }
+
+ const permissionMapper: Record<RNPermissionsStatus, PermissionStatus> = {
+  granted: "granted",
+  denied: "denied",
+  blocked: "blocked",
+  unavailable: "unavailable",
+  limited: 'limited',
+};
+
+ return permissionMapper[status] ?? 'unavailable';
+
 }
